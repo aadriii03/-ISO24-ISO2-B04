20510cc0f96fe0dc21475d83cd8f22720c314bed
JeronimodlaCruz <jeronimo.cruz@alu.uclm.es>
2024-11-13 18:24:44 +0100

    Clases en java it7

---
+++ CambioDato.java
@@ -0,0 +1,13 @@
+public class CambioDato {
+
+	private String id;
+	private Date fechaCambio;
+	private String tipoCambio;
+	private String descripcionCambio;
+
+	public void registrarCambio() {
+		// TODO - implement CambioDato.registrarCambio
+		throw new UnsupportedOperationException();
+	}
+
+}
