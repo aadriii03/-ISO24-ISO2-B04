27d2b4dd149c888d1f4c2dff34eea97eaa5456fb
JeronimodlaCruz <jeronimo.cruz@alu.uclm.es>
2024-12-09 18:56:53 +0100

    Implementación correspondiente a it1 cliente

---
+++ c1_proponer_datos/src/main/java/org/c1_proponer_datos/IUCrearMetadatos.java
@@ -0,0 +1,17 @@
+package org.c1_proponer_datos;
+
+public class IUCrearMetadatos {
+
+	private String nombreInterfaz;
+
+	public void mostrarFormulario() {
+		// TODO - implement IUCrearMetadatos.mostrarFormulario
+		throw new UnsupportedOperationException();
+	}
+
+	public void guardarMetadatos() {
+		// TODO - implement IUCrearMetadatos.guardarMetadatos
+		throw new UnsupportedOperationException();
+	}
+
+}
